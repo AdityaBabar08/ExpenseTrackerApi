3bd1c2f120c83d1d25547e3798ea83a288790d2b
Aditya Babar <babaraditya02@gmail.com>
2026-08-30 20:39:39 +0530

    Refactored the endpoints into their own respective class and cleaned up Program.cs

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,16 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Text;
 using ExpenseTracker.Data;
-using ExpenseTracker.Dtos;
-using ExpenseTracker.Models;
+using ExpenseTracker.Endpoints;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.IdentityModel.Tokens;
-using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -50,132 +43,13 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapPost("/register", async (RegisterUserDto registerUser, AppDbContext dbContext) =>
-{
 
-    if (await dbContext.Users.AnyAsync(u => u.UserName == registerUser.Username || u.Email == registerUser.Email))
-    {
-        return Results.Conflict("User already exists");
-    }
 
-    var newUser = new User
-    {
-        UserName = registerUser.Username,
-        Email = registerUser.Email,
-        PasswordHash = BCrypt.Net.BCrypt.HashPassword(registerUser.Password),
-
-    };
-
-    await dbContext.Users.AddAsync(newUser);
-    await dbContext.SaveChangesAsync();
-
-    return Results.Ok(new ResponseUserDto
-    {
-        Id = newUser.UserId,
-        Username = newUser.UserName,
-        Email = newUser.Email
-
-    });
-
-});
-
-app.MapPost("/login", async (LoginDto loginDto, AppDbContext dbContext, IConfiguration config) =>
-{
-    string tokenString = string.Empty;
-    var storedUser = await dbContext.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username);
-    if (storedUser is null)
-    {
-        return Results.Unauthorized();
-    }
-    bool isValid = BCrypt.Net.BCrypt.Verify(loginDto.Password, storedUser.PasswordHash);
-    if (isValid)
-    {
-        var jwtConfigs = config.GetSection("jwt");
-        var claims = new[]
-        {
-            new Claim(ClaimTypes.NameIdentifier, storedUser.UserId.ToString()),
-            new Claim(ClaimTypes.Name, storedUser.UserName)
-        };
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfigs["key"]));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        var token = new JwtSecurityToken(
-            issuer: jwtConfigs["Issuer"],
-            audience: jwtConfigs["audience"],
-            claims: claims,
-            expires: DateTime.UtcNow.AddDays(double.Parse(jwtConfigs["expireDays"])),
-            signingCredentials: creds
-        );
-
-        tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-        return Results.Ok(new { tokenString });
-    }
-    else
-    {
-        return Results.Unauthorized();
-    }
-
-});
-
+//-----------------Auth Endpoints----------------//
+app.MapAuthEndpoints();
 
 //-----------------Category Endpoints----------------//
+app.MapCategoryEndpoints();
 
-app.MapGet("/categories", async (AppDbContext dbContext, ClaimsPrincipal user) =>
-{
-    int userId = int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-    var query = dbContext.Categories.Where(t => t.UserId == userId);
-    return await query.ToListAsync();
-}).RequireAuthorization();
-
-app.MapPost("/categories", async (CategoryCreateDto categoryCreateDto, AppDbContext dbContext, ClaimsPrincipal user) =>
-{
-    int userId = int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-    bool exist = await dbContext.Categories.AnyAsync(c => c.Name.ToLower() == categoryCreateDto.CategoryName.ToLower() && c.UserId == userId);
-    if (exist)
-    {
-        return Results.Conflict("Category already exist");
-    }
-    Category newCategory = new()
-    {
-        Name = categoryCreateDto.CategoryName,
-        UserId = userId
-    };
-    await dbContext.Categories.AddAsync(newCategory);
-    await dbContext.SaveChangesAsync();
-    return Results.Created($"/categories/{newCategory.CategoryId}", newCategory);
-
-}).RequireAuthorization();
-
-app.MapPatch("/categories/{id}", async (int id, CategoryUpdateDto categoryUpdateDto, AppDbContext dbContext, ClaimsPrincipal user) =>
-{
-
-    int userId = int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-    var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(c => c.CategoryId == id && c.UserId == userId);
-    if (existingCategory is null)
-    {
-        return Results.NotFound("Category of this Id not found or doesn't exist");
-    }
-    existingCategory.Name = categoryUpdateDto.UpdatedCategory;
-
-    await dbContext.SaveChangesAsync();
-    return Results.Ok("Category updated successfully");
-
-}).RequireAuthorization();
-
-app.MapDelete("/categories/{id}", async (int id, AppDbContext dbContext, ClaimsPrincipal user) =>
-{
-    int userId = int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-    var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(c => c.CategoryId == id && c.UserId == userId);
-    if (existingCategory is null)
-    {
-        return Results.NotFound("Category of this Id not found or doesn't exist");
-    }
-    dbContext.Categories.Remove(existingCategory);
-
-    await dbContext.SaveChangesAsync();
-    return Results.NoContent();
-
-}).RequireAuthorization();
 
 app.Run();
